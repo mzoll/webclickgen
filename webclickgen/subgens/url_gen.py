98df8fccceb0f86992291fe61c0d0eba9b2caea5
mzoll <marcel.zoll.physics@gmail.com>
2019-02-06 21:14:13 +0100

    there is a referer now

--- webclickgen/subgens/url_gen.py
+++ webclickgen/subgens/url_gen.py
@@ -5,8 +5,13 @@ import numpy as np
 class URLGenerator(object):
     class URLGen_child(object):
         """ Subclass to URLGenerator which can be spawned through URLGenerator.get_child() and reponds to step() and get_dict() """
-        def __init__(self, domain_structure_dict, land_main_prob=0.5, step_levelout_prob=0.3):
+        def __init__(self,
+                     domain_structure_dict,
+                     referer_structure_dict,
+                     land_main_prob=0.5,
+                     step_levelout_prob=0.3):
             self.domain_structure_dict = domain_structure_dict
+            self.referer_structure_dict = referer_structure_dict
             self.land_main_prob = land_main_prob
             self.step_levelout_prob = step_levelout_prob
 
@@ -25,11 +30,15 @@ class URLGenerator(object):
                                   '.' + self.domain_structure_dict['host'] + '.' + self.domain_structure_dict['topdomain'] + \
                                   '/' + self._p0l + '/' + d + '.' + self.domain_structure_dict[
                                       'document_ext']
+            #dice the referer
+            self.referer = np.random.choice(list(self.referer_structure_dict.keys()), size=1, p=list(self.referer_structure_dict.values()))
+
 
         def soft_reset(self):
             self.reset()
 
         def step(self):
+            self.referer = self.url
             if np.random.rand() < self.step_levelout_prob or self._p0l == '.':
                 self._p0l = np.random.choice(self.domain_structure_dict['path0_levels'])
                 d = np.random.choice(self.domain_structure_dict['path0_document_tree'][self._p0l])
@@ -45,7 +54,7 @@ class URLGenerator(object):
                                     'document_ext']
 
         def get_dict(self):
-            return {'url': self.url}
+            return {'url': self.url, 'referer': self.referer}
 
     def __init__(self,  num_path0_levels=10, MIN_NUM_DOC_LEVELS=5, MAX_NUM_DOC_LEVELS=10, **child_kwargs):
         """ Gernerates a deep Website domain structure, which can be used to create abitrary click-patterns
@@ -63,25 +72,43 @@ class URLGenerator(object):
         self.MAX_NUM_DOC_LEVELS = MAX_NUM_DOC_LEVELS
         self.child_kwargs = child_kwargs
 
-        self.domain_structure_gendict = {'protocol': 'https', 'subdomain': 'www', 'host': 'hostname', 'topdomain': 'com',
-                                    'path0_levels': [], 'path0_document_tree': {}, 'document_ext': 'html'}
-        self._urls = []
+        self.domain_structure_gendict, _ = self.gen_domain_structure_dict(
+                self.num_path0_levels,
+                self.MIN_NUM_DOC_LEVELS,
+                self.MAX_NUM_DOC_LEVELS)
 
+        self.referer_structure_dict = self.gen_referer_structure_dict()
+
+    def gen_domain_structure_dict(self, num_path0_levels,  MIN_NUM_DOC_LEVELS, MAX_NUM_DOC_LEVELS):
+        domain_structure_gendict = {'protocol': 'https', 'subdomain': 'www', 'host': 'hostname', 'topdomain': 'com',
+                                    'path0_levels': [], 'path0_document_tree': {}, 'document_ext': 'html'}
+        urls = []
         for i in range(num_path0_levels):
             p0l = gen_nletter_word(3)
-            self.domain_structure_gendict['path0_levels'].append(p0l)
-            self.domain_structure_gendict['path0_document_tree'][p0l] = []
+            domain_structure_gendict['path0_levels'].append(p0l)
+            domain_structure_gendict['path0_document_tree'][p0l] = []
             for j in range(np.random.choice(range(MIN_NUM_DOC_LEVELS, MAX_NUM_DOC_LEVELS))):
                 doc_name = gen_nletter_word(3)
-                self.domain_structure_gendict['path0_document_tree'][p0l].append(doc_name)
-                self._urls.append(self.domain_structure_gendict['protocol'] + '://' + self.domain_structure_gendict['subdomain'] + \
-                            '.' + self.domain_structure_gendict['host'] + '.' + self.domain_structure_gendict['topdomain'] + \
-                            '/' + p0l + '/' + doc_name + '.' + self.domain_structure_gendict['document_ext'])
+                domain_structure_gendict['path0_document_tree'][p0l].append(doc_name)
+                urls.append(domain_structure_gendict['protocol'] + '://' + domain_structure_gendict['subdomain'] + \
+                            '.' + domain_structure_gendict['host'] + '.' + domain_structure_gendict['topdomain'] + \
+                            '/' + p0l + '/' + doc_name + '.' + domain_structure_gendict['document_ext'])
+        return domain_structure_gendict, urls
+
+    def gen_referer_structure_dict(self):
+        return {'': 0.1,
+                'http://other.place.com/': 0.1,
+                'https://www.facebook.com/': 0.1,
+                'https://www.bing.com/': 0.2,
+                'https://www.yahoo.com/': 0.1,
+                'https://www.google.com/': 0.4
+        }
+
 
     def get_child(self, **kwargs):
         param_dict = self.child_kwargs.copy()
         param_dict.update(kwargs)
-        return self.URLGen_child(self.domain_structure_gendict, **param_dict)
+        return self.URLGen_child(self.domain_structure_gendict, self.referer_structure_dict, **param_dict)
 
 
 def gen_nidletter_words(nletters):
